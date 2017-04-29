cecb9d3f4fd426524c3a1a043eba39073d63f5b3
Uwe Niethammer <uwe@T450p>
2017-04-29 02:31:51 +0200

    cleanup

--- build.sh
+++ build.sh
@@ -52,38 +52,14 @@ function make_cm_kernel {
 		cp -vr $ZIMAGE_DIR/$KERNEL $REPACK_DIR/tmp/anykernel
 }
 
-function make_aosp_kernel {
-		HC_VER="$BASE_HC_VER$VER-AOSP-UBERTC-6.x"
-		echo "[....Building `echo $HC_VER`....]"
-		make $DEFCONFIG
-		make $THREAD
-		cp -vr $ZIMAGE_DIR/$KERNEL $REPACK_DIR/tmp/anykernel
-}
-
-function make_special_aosp_kernel {
-		HC_VER="$BASE_HC_VER$VER-AOSP-UBERTC-6.x-CPUSET"
-		echo "[....Building `echo $HC_VER`....]"
-		make $DEFCONFIG
-		make $THREAD
-		cp -vr $ZIMAGE_DIR/$KERNEL $REPACK_DIR/tmp/anykernel
-}
 
 function git_addback_cm_patch {
 		branch_name=$(git symbolic-ref -q HEAD)
 		branch_name=${branch_name##refs/heads/}
 		branch_name=${branch_name:-HEAD}
 		git checkout -b temp-for-making-cm-build
-		git revert fe4d203c80f7cb2d491af21ea19fe1a2bf69265c --no-edit
 }
 
-function git_addback_cpuset_commits {
-		branch_name=$(git symbolic-ref -q HEAD)
-		branch_name=${branch_name##refs/heads/}
-		branch_name=${branch_name:-HEAD}
-		git checkout -b temp-for-making-special-build
-		git revert 9474793a59bf888faa783d8556e56dc12b2b5831 --no-edit
-		git revert d09522ae74613f29543a669a9c051dcb2037c331 --no-edit
-}
 
 function git_switch_to_previous_branch {
 		git checkout $branch_name
@@ -107,7 +83,7 @@ function copy_dropbox {
 DATE_START=$(date +"%s")
 
 echo -e "${green}"
-echo "HellSpawn-N4 Nougat Kernel Creation Script:"
+echo "N4 Nougat Kernel Creation Script:"
 echo
 
 echo "---------------"
@@ -123,7 +99,6 @@ echo "-----------------"
 echo -e "${restore}"
 
 
-
 		HC_VER="$BASE_HC_VER$VER"
 		echo -e "${green}"
 		echo
@@ -133,32 +108,7 @@ echo -e "${restore}"
 		clean_all
 		echo -e "${green}"
 		echo
-		make_aosp_kernel
-		echo
-		echo -e "${restore}"
-		echo -e "${green}"
-		echo
-		echo "[....Make `echo $HC_VER`.zip....]"
-		echo
-		echo -e "${restore}"
-		make_zip
-		echo -e "${green}"
-		echo
-		echo "[.....Moving `echo $HC_VER`.....]"
-		echo
-		echo -e "${restore}"
-		copy_dropbox
-
-		HC_VER="$BASE_HC_VER$VER"
-		echo -e "${green}"
-		echo
-		echo "[..........Cleaning up..........]"
-		echo
-		echo -e "${restore}"
-		clean_all
-		echo -e "${green}"
-		echo
-		git_addback_cm_patch
+	#	git_addback_cm_patch
 		make_cm_kernel
 		echo
 		echo -e "${restore}"
